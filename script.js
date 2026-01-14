0df38a0e874b5b7ea8542b424df57fa31b4bf850
marcocentofanti <marcocentofanti@hotmail.it>
2026-01-14 12:26:25 +0100

    post correction

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore.
 */
 
-/*
+
 const bici = [
   { nome: "a", peso: 5},
   { nome: "b", peso: 41},
@@ -16,16 +16,47 @@ const bici = [
   { nome: "f", peso: 19},
 ]
 
-let pesoMaggiore = 0
+let pesoMaggiore = 100
 let indexBiciPesante
 for (let index = 0; index < bici.length; index++) {
-if (bici[index].peso > pesoMaggiore) {
+if (bici[index].peso < pesoMaggiore) {
   pesoMaggiore = bici[index].peso
   indexBiciPesante = index
  }
 }
 console.log(bici[indexBiciPesante])
-*/
+
+// ESERCIZIO POST CORREZIONE
+
+const bicycles = [
+  { nome: "a", peso: 5},
+  { nome: "b", peso: 41},
+  { nome: "c", peso: 45},
+  { nome: "d", peso: 24},
+  { nome: "e", peso: 30},
+  { nome: "f", peso: 19},
+]
+
+function getLighterBike(bicycles) {
+  if (bicycles.length <= 0) {
+    return {};
+  }
+  let lightestBike =  bicycles[0];
+
+  for (let i = 1; i < bicycles.length; i++){
+    if (lightestBike.peso > bicycles[i].peso) {
+      lightestBike = bicycles[i];
+    }
+  } 
+  return lightestBike
+}
+
+const lightestBike = getLighterBike(bicycles)
+console.log(getLighterBike(bicycles))
+
+document.getElementById("lighterBike").textContent = `${lightestBike.nome}: Peso:${lightestBike.peso}`
+
+
 
 /*
 Snack 2
@@ -40,7 +71,7 @@ function numeroRandom(max) {
   return Math.floor(Math.random() * max)
 }
 
-const teams = [
+const squadre = [
   { name: "Inter", 
     points: 0,
     foulsSuffered: 0,},
@@ -55,24 +86,77 @@ const teams = [
     foulsSuffered: 0,},
 ]
 
-console.log(teams)
+console.log(squadre)
 
 
-for (let index2 = 0; index2 < teams.length; index2++ ) {
-  for (let key in teams[index2]) {
+for (let index2 = 0; index2 < squadre.length; index2++ ) {
+  for (let key in squadre[index2]) {
     if(key === "points") {
-      teams[index2].points = numeroRandom(100)
+      squadre[index2].points = numeroRandom(100)
     } else {
       if(key === "foulsSuffered") {
-        teams[index2].foulsSuffered = numeroRandom(10)
+        squadre[index2].foulsSuffered = numeroRandom(10)
       }}}}
       
       
       const Extraction =[]
 
-    for (let i = 0; i < teams.length; i++){
-      Extraction.push(teams[i].name);
-      Extraction.push(teams[i].foulsSuffered);
+    for (let i = 0; i < squadre.length; i++){
+      Extraction.push(squadre[i].name);
+      Extraction.push(squadre[i].foulsSuffered);
     }
 
     console.log(Extraction)
+
+
+    // ESERCIZIO POST CORREZIONE
+
+
+    const randomNumber = (min, max) => {
+      max - Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    const teams = [
+      {
+        nome: "PSG",
+        puntiFatti: 0,
+        falliSubiti: 0,
+      },
+      {
+        nome: "Ajax",
+        puntiFatti: 0,
+        falliSubiti: 0,
+      },
+      {
+        nome: "Manchester City",
+        puntiFatti: 0,
+        falliSubiti: 0,
+      },
+      {
+        nome: "Real Madrid",
+        puntiFatti: 0,
+        falliSubiti: 0,
+      },
+    ]
+
+for (let i = 0; i < teams.length; i++) {
+  currentTeam = teams[i];
+
+  currentTeam.puntiFatti = randomNumber(0, 100);
+  currentTeam.falliSubiti = randomNumber(10, 200);
+}
+
+console.log(teams)
+
+const teamsWithoutPoints = []
+
+for (let i = 0; i < teams.length; i++) {
+  const currentTeam = {
+    nome: teams[i].nome,
+    falliSubiti: teams[i].falliSubiti,
+  }
+}
+
+teamsWithoutPoints.push(currentTeam)
+console.log(teams)
